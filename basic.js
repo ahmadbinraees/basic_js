0c68c09a0b40e3de92a58410a641c71465b49801
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-02 11:35:51 +0600

    string type variable added

--- basic.js
+++ basic.js
@@ -14,3 +14,15 @@ var foodQty = 15;
 console.log("This is Something price = ",somethingPrice);
 console.log("This is Something someone Age = ",someoneAge);
 console.log("This is Food Quantity = ",foodQty);
+
+
+// string var
+var someoneName = "Tom";
+var BookName = "Triagle";
+var someOneAddress = "Mokey Area , banana street - 2124";
+
+console.log("This is SomeOne Name : " , someoneName);
+console.log("This is a Book Name : " , BookName);
+console.log("This is SomeOne Address : " , someOneAddress);
+
+
