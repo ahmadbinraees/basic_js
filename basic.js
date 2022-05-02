2a5548ec7ea2ba3e8993e3bbf8a13a5c37be469d
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-02 11:15:24 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,16 @@
+// declaring a variable
+/*
+there is three type of variable.
+1- numeric type variable
+2- string type variable
+3- boolean type variable
+*/
+
+// numeric var 
+var somethingPrice = 25;
+var someoneAge = 27;
+var foodQty = 15;
+
+console.log("This is Something price = ",somethingPrice);
+console.log("This is Something someone Age = ",someoneAge);
+console.log("This is Food Quantity = ",foodQty);
