1813abe4a6d5b1f673f40143c3f219c8adfdb144
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-23 11:42:57 +0600

    Names variables added

---
+++ info.js
@@ -0,0 +1,12 @@
+// this is my info in variable
+
+
+const firstName = "Ahmadullah";
+const lastName = "Sheikh";
+const titleName = "Bin Raees";
+const nickName = "Tonmoy";
+
+let formalName = firstName + lastName;
+let fullName = firstName + nickName;
+let imamName = firstName + lastName;
+let withFather = firstName + titleName;
