4aa737290a7ec15481103fa2e14f017b854001c6
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-23 11:56:05 +0600

    white space added between names

--- info.js
+++ info.js
@@ -6,7 +6,12 @@ const lastName = "Sheikh";
 const titleName = "Bin Raees";
 const nickName = "Tonmoy";
 
-let formalName = firstName + lastName;
-let fullName = firstName + nickName;
-let imamName = firstName + lastName;
-let withFather = firstName + titleName;
+let formalName = firstName + " " + lastName;
+let fullName = firstName + " " + nickName;
+let imamName = firstName + " " + lastName;
+let withFather = firstName + " " + titleName;
+
+console.log(formalName);
+console.log(fullName);
+console.log(imamName);
+console.log(withFather);
