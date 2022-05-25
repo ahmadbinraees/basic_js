820a2abd151ea84a9ac1862a4a2aff587ab384a1
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-26 00:46:06 +0600

    more data added in tounData object

--- object.js
+++ object.js
@@ -3,4 +3,8 @@ let tonuData = {
     lastName: "Seikh",
     nickName: "Tonmoy",
     titleName: "Bin Raees",
+    age: 24,
+    mobile: 8801303281462,
+    gmail:"ahmadullahbinraees@gmail.com",
+    fbLink: "https://www.facebook.com/ahmadbinraees/",
 };
