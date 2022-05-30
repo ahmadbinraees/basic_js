4171c38170ceccf55fc7cef198330c201c77bea1
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-31 00:40:00 +0600

    float and integer added

---
+++ float-integer.js
@@ -0,0 +1,10 @@
+// float = decimal (and int)
+// integer = without decimal
+
+let decNum = 2.5;
+
+var floatIt = parseFloat(decNum);
+console.log(floatIt);
+
+var intIt = parseInt(decNum);
+console.log(intIt);
