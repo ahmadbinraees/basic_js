edde057165c6bf75c62f0a8edc748e7ca3555bd1
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-06-04 23:13:10 +0600

    recap point comment out

---
+++ recap.js
@@ -0,0 +1,8 @@
+// what is variable?
+// how to declare a variable?
+// variable name naming convention
+// Simple Mathematical operations in JavaScript
+// (advanced) Mathematical operation shorthand
+// (advanced) Integer float parseInt parseFloat type conversion
+// Different variable types and use toFixed with parseFloat
+// remainder modulus
