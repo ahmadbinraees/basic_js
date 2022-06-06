d4fa0b5da86f14a1aa6124ef79750d4869283538
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-06-06 07:41:53 +0600

    Variable explanation comment out

--- recap.js
+++ recap.js
@@ -6,3 +6,27 @@
 // (advanced) Integer float parseInt parseFloat type conversion
 // Different variable types and use toFixed with parseFloat
 // remainder modulus
+
+
+/* ------------------------------------------------------------------------------------------------
+
+''''''''''''''''''''''''''''''''''''''''''''' VARIABLE ''''''''''''''''''''''''''''''''''''''''''''
+
+A Variable is a container what is contain data, info and values.
+
+- five things to do for declare a variable.
+- 1-- write variable key like "var", "let", "const".
+- 2-- give a name to this variable.
+- 3-- then give an equals sign.
+- 4-- then give a value.
+- 5-- and last, it has to end up with a clone " ; " .
+
+'''''''''''''''''''''''''''''''''''''''' TYPE OF VARIABLE ''''''''''''''''''''''''''''''''''''''''''
+
+There is three types of variable :- 
+
+- 1-- Numerical
+- 2-- String
+- 3-- Boolean
+
+---------------------------------------------------------------------------------------------------*/
